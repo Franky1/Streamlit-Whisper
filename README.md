eee5ab3e79b324d3a36188ca8f2316dfe2bd1bf6
Arvid Teichtmann <arvid.teichtmann@gmx.de>
2023-03-01 19:59:12 +0100

    update

--- README.md
+++ README.md
@@ -6,15 +6,24 @@ Streamlit demo project for openai whisper.
 
 ## Status
 
-> Work in progress - Not finished - Last changed: 2023-02-28
+> Work in progress - Not finished - Last changed: 2023-03-01
+
+## Issues
+
+- Dependencies are very heavy (whisper, pytorch, transformers, etc.) and take a long time to install.
+- Model is not pre-downloaded and very huge, so it takes a long time to download the first time.
+- Transcribe takes a long time to run.
+- Uploaded file is stored in root directory, so it is not deleted after the app is closed.
 
 ## ToDo
 
-- [ ] Test app locally with docker
+- [x] Test app locally with docker
+- [ ] Use temporary directory for uploaded file
 - [ ] Add audio recording to app
 - [ ] Test pre-download of model
-- [ ] Use Git-LFS for model
+- [ ] Use Git-LFS for model(?)
 - [ ] Try deployment on streamlit cloud
+- [ ] Improve layout of the frontend
 
 ## Resources
 
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,51 +1,56 @@
+from pathlib import Path
 import streamlit as st
 import whisper
 
 
-@st.cache_resource
-def load_model(name: str="medium"):
+@st.cache_resource(show_spinner=False)
+def load_model(name: str):
     model = whisper.load_model(name)
     return model
 
 
-@st.cache_data
-def get_audio_file_details(file):
-    file_details = {"FileName": file.name,
-                    "FileType": file.type, "FileSize": file.size}
-    return file_details
+@st.cache_data(show_spinner=False)
+def transcribe(_model, audio_file):
+    transcription = _model.transcribe(audio_file, fp16=False)
+    return transcription["text"]
 
 
-model = load_model()
 header = st.container()
 explain = st.container()
-pre = st.container()
 
 with header:
     st.title("Convert Speech to Text")
-    st.subheader(
-        "Note: This Model can Only Transcribe from English Speech to English Text")
-    st.write("1. Upload Audio File \n 2. Wait till Transcribe Button Appears \n 3. Click on transcribe button and wait till result generates \n 4. Can  download the text in .txt format \n 5. If you Liked it then Press the Button.")
-    st.subheader("Available Models")
-    st.table(whisper.available_models())
+    # st.write("Note: This Model can only transcribe from English speech to English text")
+    st.markdown('''
+                1. Upload Audio File
+                2. Wait until `Transcribe Audio` button appears
+                3. Click on `Transcribe Audio` button and wait until result generated
+                4. You can download the result in .txt format
+                ''')
+
+with st.spinner("Loading Model for the first time, please wait..."):
+    model = load_model(name="small")
 
 with explain:
-    audio_file = st.file_uploader("Upload Audio", type=["wav", "mp3", "m4a"])
+    audio_file = st.file_uploader("Upload Audio", type=["wav", "mp3", "m4a", "ogg"])
+    # FIXME: Write to temp file instead of root folder
+    if audio_file is not None:
+        audio_file_path = Path(audio_file.name)
+        with open(audio_file.name, "wb") as f:
+            f.write(audio_file.getvalue())
 
     if st.sidebar.button("Transcribe Audio"):
         if audio_file is not None:
             st.sidebar.success("Transcribing Audio")
-            transcription = model.transcribe(audio_file.name)
+            with st.spinner("Running Transcribe, please wait..."):
+                transcription = transcribe(model, audio_file_path.name)
             st.sidebar.success("Transcription Complete")
-            result = transcription["text"]
-            st.markdown(result)
+            st.markdown(transcription)
             st.download_button(label='Download Text',
-                            data=result, file_name="Result.txt")
+                        data=transcription,
+                        file_name=audio_file_path.with_suffix(".txt").name)
         else:
             st.sidebar.error("Please Upload an Audio File")
 
     st.sidebar.header("Play Original Audio File")
     st.sidebar.audio(audio_file)
-
-with pre:
-    if st.sidebar.button("Press if you Like It"):
-        st.balloons()
