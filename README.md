9dc6a52ca9f5ff2a7921e9945cb1ad5a22003817
Franky1 <arvid.teichtmann@gmx.de>
2023-03-04 22:56:59 +0100

    update

--- README.md
+++ README.md
@@ -1,32 +1,48 @@
-# Streamlit Openai Whisper
+# Streamlit Openai Whisper 🗣️
 
 [![Streamlit](https://img.shields.io/badge/Go%20To-Streamlit%20Cloud-red?logo=streamlit)](https://franky1-whisper-streamlit-app-ode4s8.streamlit.app/)
 
-Streamlit demo project for openai whisper.
+Streamlit demo project for Openai Whisper.
 
-## Status
+## Status ✔️
 
-> Work in progress - Not finished - Last changed: 2023-03-03
+> Streamlit App works - Has some issues - Last changed: 2023-03-04
 
-## Issues
+## Issues 🚩
 
-- Dependencies are very heavy (whisper, pytorch, transformers, etc.) and take a long time to install.
+- Dependencies of `whisper` are very heavy (pytorch, transformers, cuda...) and take a long time to install.
 - Model is not pre-downloaded and very huge, so it takes a long time to download the first time.
-- Larger models seems to crash on streamlit cloud, probably due to memory limits.
+- Larger models above the `small` model seems to crash on streamlit cloud, probably due to memory limits.
 - The largest model that seems to run locally and on streamlit cloud is `small`.
+- Usage of Git-LFS for pre-downloaded model also does not make sense, because at least in the free version of GitHub, you only get 1GB of storage space and 1GB of bandwidth per month.
 - Transcribe takes a long time to run.
+- The audiorecorder component is not working.
 
-## ToDo
+## ToDo ☑️
 
 - [x] Test app locally with docker
+- [x] Test app locally within virtual environment
 - [x] Use temporary directory for uploaded file
 - [x] Try deployment on streamlit cloud
 - [x] Try larger models
-- [ ] Add audio recording to app
-- [ ] Try pre-download of model(?)
-- [ ] Use Git-LFS for model(?)
+- [x] Add custom audio recording component to streamlit app
+- [ ] Fix and test audiorecorder issue
+- [ ] Try other audio recording components
 - [ ] Improve layout of the frontend
+- [ ] Restructure file structure
 
-## Resources
+## Resources 📚
 
-- <https://github.com/openai/whisper>
+- Streamlit ❤️
+  - <https://docs.streamlit.io/>
+- Openai Whisper ❤️
+  - <https://github.com/openai/whisper>
+- Streamlit Custom Components for Audio Recording
+  - streamlit-webrtc 🤷‍♂️
+    - <https://github.com/whitphx/streamlit-webrtc>
+  - streamlit-audiorecorder 🤷‍♂️
+    - <https://github.com/theevann/streamlit-audiorecorder>
+  - audio-recorder-streamlit 🤷‍♂️
+    - <https://github.com/Joooohan/audio-recorder-streamlit>
+  - streamlit_audio_recorder 🤔
+    - <https://github.com/stefanrmmr/streamlit_audio_recorder>
--- streamlit_app.py
+++ streamlit_app.py
@@ -8,9 +8,13 @@ from pathlib import Path
 
 import streamlit as st
 import whisper
+from audiorecorder import audiorecorder
 
 
-st.set_page_config(page_title='Openai Whisper Transcriber', page_icon='📜', layout="centered", initial_sidebar_state="expanded")
+st.set_page_config(page_title='Openai Whisper Transcriber',
+                page_icon='📜',
+                layout="wide",
+                initial_sidebar_state="expanded")
 
 
 # apply custom css
@@ -18,14 +22,13 @@ with open(Path('utils/style.css')) as css:
     st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
 
 
-@st.cache_resource(ttl=60*60*24)
+@st.cache_resource(ttl=60*60*24, show_spinner=False)
 def cleanup_tempdir() -> None:
     '''Cleanup temp dir for all user sessions.
     Filters the temp dir for uuid4 subdirs.
     Deletes them if they exist and are older than 1 day.
     '''
     deleteTime = datetime.now() - timedelta(days=1)
-    # compile regex for uuid4
     uuid4_regex = r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
     uuid4_regex = re.compile(uuid4_regex)
     tempfiledir = Path(tempfile.gettempdir())
@@ -36,6 +39,20 @@ def cleanup_tempdir() -> None:
             itemTime = datetime.fromtimestamp(subdir.stat().st_mtime)
             if itemTime < deleteTime:
                 shutil.rmtree(subdir)
+
+
+def get_all_uuid4_subdirs_from_tempdir() -> None:
+    '''Get all uuid4 subdirs from temp dir for all user sessions.'''
+    uuid4_regex = r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
+    uuid4_regex = re.compile(uuid4_regex)
+    tempfiledir = Path(tempfile.gettempdir())
+    if tempfiledir.exists():
+        subdirs = [x for x in tempfiledir.iterdir() if x.is_dir()]
+        subdirs_match = [x for x in subdirs if uuid4_regex.match(x.name)]
+        subdirs_match = [str(x) for x in subdirs_match]
+        return subdirs_match
+    else:
+        return None
 
 
 @st.cache_data(show_spinner=False)
@@ -64,6 +81,20 @@ def store_file_in_tempdir(tmpdirname: Path, uploaded_file: BytesIO) -> Path:
     return tmpfile
 
 
+def write_file_in_tempdir(tmpdirname: Path, uploaded_file: bytearray, filename: str) -> Path:
+    '''Write file in temp dir and return path to it
+    params: tmpdirname: Path to temp dir
+            uploaded_file: bytearray
+            filename: str
+    returns: Path to stored file
+    '''
+    # store file in temp dir
+    tmpfile = tmpdirname.joinpath(filename)
+    with open(tmpfile, 'wb') as f:
+        f.write(uploaded_file)
+    return tmpfile
+
+
 @st.cache_resource(show_spinner=False)
 def load_model(name: str):
     model = whisper.load_model(name)
@@ -80,8 +111,12 @@ if __name__ == "__main__":
     cleanup_tempdir()  # cleanup temp dir from previous user sessions
     tmpdirname = make_tempdir()  # make temp dir for each user session
 
-    st.title("Convert Speech to Text")
-    # st.write("Note: This Model can only transcribe from English speech to English text")
+    lcol, ccol, rcol = st.columns([1, 4, 1], gap="small")
+    with ccol:
+        st.title("Convert Speech to Text 🗣️ ➡️ 📜")
+        # st.write("Note: This Model can only transcribe from English speech to English text")
+        # st.info(tmpdirname)
+        # st.table(get_all_uuid4_subdirs_from_tempdir())
 
     with st.sidebar:
         st.image('resources/openai2.png')
@@ -94,35 +129,47 @@ if __name__ == "__main__":
                             - <https://github.com/Franky1/whisper>
                             ''')
         st.markdown('''---''')
-        st.header("How to use this app")
+        st.header("How to use this app 👈")
         st.markdown('''
-                    1. Upload Audio File
-                    2. Wait until "**Transcribe Audio**" button appears
-                    3. Click on "**Transcribe Audio**" button and wait until result generated
-                    4. Result can be downloaded in .txt format
+                    1. Upload Audio File or Record Audio
+                    2. Click on "**Transcribe Audio**" button and wait until result generated
+                    3. Result can be downloaded in .txt format
                     ''')
         st.markdown('''---''')
 
-    with st.spinner("Loading Openai Model for the first time, please wait..."):
-        model = load_model(name="small")
+    with ccol:
+        with st.spinner("Loading Openai Model for the first time, please wait..."):
+            model = load_model(name="small")
 
-    st.subheader("Upload Audio File")
-    audio_file = st.file_uploader("Upload Audio", type=["wav", "mp3", "m4a", "ogg"])
-    if audio_file is not None:
-        tmpfile = store_file_in_tempdir(tmpdirname, audio_file)
-        audio_file_path = Path(audio_file.name)
-        st.sidebar.header("Play Original Audio File")
+        col1, col2 = st.columns([5, 3], gap="large")
+        with col1:
+            st.subheader("Upload Audio File 🎵")
+            audio_file = st.file_uploader("Upload Audio", type=["wav", "mp3", "m4a", "ogg"], label_visibility="collapsed")
+        with col2:
+            st.subheader("Record Audio 🎤")
+            audio = audiorecorder("Click to record", "Recording...")
+
+        if audio_file is not None:
+            tmpfile = store_file_in_tempdir(tmpdirname, audio_file)
+            audio_file_path = Path(audio_file.name)
+        elif len(audio) > 0:
+            audio_file_bytes = audio.tobytes()
+            tmpfile = write_file_in_tempdir(tmpdirname, audio_file_bytes, filename="audio.mp3")
+            audio_file_path = Path("audio.mp3")
+        else:
+            st.sidebar.warning("Please Upload an Audio File")
+            st.stop()
+
+        st.sidebar.header("Play Original Audio File 🔊")
         st.sidebar.audio(audio_file)
-        if st.sidebar.button("Transcribe Audio"):
+        if st.sidebar.button("Transcribe Audio ✍️"):
             st.sidebar.info("Transcribing Audio...")
             with st.spinner("Running Transcribe, please wait..."):
                 transcription = transcribe(model, str(tmpfile.resolve()))
             st.sidebar.success("Transcription Complete")
-            st.subheader("Transcription Result")
+            st.subheader("Transcription Result ✍️")
             st.success(audio_file.name)
             st.markdown(transcription)
-            st.download_button(label='Download Text',
+            st.download_button(label='Download Text File ',
                         data=transcription,
                         file_name=audio_file_path.with_suffix(".txt").name)
-    else:
-        st.sidebar.warning("Please Upload an Audio File")
